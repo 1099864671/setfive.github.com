4af913db5b1a9c6d2ff87c0d020ee1722464c0c0
Ashish Datta <ashish@setfive.com>
2013-03-06 23:55:48 -0500

    - switched the directory - changed what goes into the squares in the startup school viz

---
+++ d3_startup_school/data.php
@@ -0,0 +1,195 @@
+<?php 
+
+$data = array();
+$lines = explode("\n", trim(file_get_contents("ssdata.csv")));
+$header = str_getcsv(array_shift( $lines ), ";");
+
+$nameKeys = array("fname", "lname", "email_address", "email_domain");
+
+if( array_key_exists("stats", $_REQUEST) ){
+    
+    $totals = array("fname" => array(), "lname" => array(), "email" => array(), "track" => array());
+    
+    foreach( $lines as $ln ){
+        
+        $obj = array();
+        foreach( str_getcsv( $ln, ";" ) as $i => $val ){
+            
+            if( !in_array($header[$i], array("fname", "lname", "email", "track")) ){
+                continue;
+            }
+            
+            $val = trim(strtolower($val));            
+            if( $header[$i] == "email" ){
+                list($addr, $val) = explode("@", $val);
+            }                        
+            
+            if( !array_key_exists($val, $totals[$header[$i]]) ){
+                $totals[ $header[$i] ][ $val ] = 0;
+            }
+            
+            $totals[ $header[$i] ][ $val ] += 1;
+        }
+        
+    }
+    
+    foreach( array_keys($totals) as $key ){
+        ksort( $totals[$key] );
+    }
+    
+    print_r($totals);
+    
+    exit(0);
+}
+
+if( array_key_exists("filters", $_REQUEST) ){
+	$trackFilters = $_REQUEST["filters"];
+}else{
+	$trackFilters = array();
+}
+
+$maxLengths = array_fill_keys( $nameKeys, 0 );
+
+foreach( $lines as $ln ){
+	
+	$obj = array();
+	foreach( str_getcsv( $ln, ";" ) as $i => $val ){
+	    
+	    if( in_array($header[$i], $nameKeys) ){	    
+		    $obj[$header[$i]] = metaphone(trim(strtolower($val)));		    
+		    $obj[$header[$i] . "_text"] = trim(strtolower($val));
+		    $maxLengths[$header[$i]] = max( $maxLengths[$header[$i]], strlen($obj[$header[$i] . "_text"]) );		    
+	    }else if( $header[$i] == "email" ){
+	        list($addr, $domain) = explode("@", $val);	        
+	        $obj["email_address"] = metaphone(trim(strtolower($addr)));
+	        $obj["email_domain"] = metaphone(trim(strtolower($domain)));
+	    }else{
+	        $obj[$header[$i]] = trim(strtolower($val));
+	    }
+	    
+	}
+	
+	if( in_array($obj["track"], $trackFilters) ){
+		$data[] = $obj;
+	}
+		
+}
+
+
+$sortKey = "fname_text";
+if( array_key_exists("sortBy", $_REQUEST) ){
+    $sortKey = $_REQUEST["sortBy"];
+}
+
+usort($data, function($a, $b) use($sortKey) {
+   return strcmp($a[ $sortKey ],  $b[ $sortKey ]); 
+});
+
+
+$maxValues = array_fill_keys($nameKeys, array(0 => 0, 1 => 0, 2 => 0));
+$payload = array();
+
+foreach( $data as $dt ){	
+	$dataEl = array();	
+	$keyedSplits = array();
+		
+	foreach( $nameKeys as $key ){
+		$dt[$key] = str_split(trim($dt[$key]));
+		$dt[$key] = array_map(function($el){return ord($el);}, $dt[$key]);
+		
+		$keyedSplits[$key] = array_chunk($dt[$key], ceil( count($dt[$key]) / 3 )) +	 array_fill_keys(array(0, 1, 2), array());
+		$keyedSplits[$key] = array_map(function($el) {
+									   return count($el) ? array_sum($el) : 0;
+							 }, $keyedSplits[$key]);
+							 
+		foreach( array_keys($maxValues[$key]) as $index ){
+			$maxValues[$key][$index] = max( $maxValues[$key][$index], $keyedSplits[$key][$index] );
+		}
+						 
+		$dataEl[$key] = $keyedSplits[$key];
+		$dataEl[$key ."_text"] = $dt[$key . "_text"];
+	}
+
+	$dataEl["track"] = $dt["track"];	
+	
+	$payload[] = $dataEl;
+}
+
+$payload = array_map(function($dt) use ($nameKeys, $maxValues, $maxLengths){
+
+	$obj = array( 0 => array(), 1 => array() );
+	
+	$fnamePercent = getLetterHSLColor( $dt["fname_text"][0] );	
+	$fnameLengthPercent = strlen($dt["fname_text"]) / $maxLengths["fname"];
+		
+	$obj[0][] = array("h" => 40, "s" => 100, "l" => $fnamePercent);
+	$obj[0][] = array("h" => 340, "s" => 100, "l" => $fnameLengthPercent);
+	$obj[0][] = getHSLColor( $dt, "fname", $maxValues );	
+
+	$lnamePercent = getLetterHSLColor( $dt["lname_text"][0] );
+	$lnameLengthPercent = strlen($dt["lname_text"]) / $maxLengths["lname"];
+	
+	$obj[1][] = array("h" => 40, "s" => 100, "l" => $lnamePercent);
+	$obj[1][] = array("h" => 340, "s" => 100, "l" => $lnameLengthPercent);
+	$obj[1][] = getHSLColor( $dt, "lname", $maxValues );	
+	
+	switch( $dt["track"] ){	    
+	    case "sales":
+	        $trackColor = array("h" => 120, "s" => .78, "l" => .75);
+	        break;	        
+	    case "prod":
+	        $trackColor = array("h" => 293, "s" => .83, "l" => .75);
+	        break;	        
+	    case "mrkt":
+	        $trackColor = array("h" => 31, "s" => .9, "l" => .75);
+	        break;	        	    
+	    default:
+	    case "dev":
+	        $trackColor = array("h" => 221, "s" => .83, "l" => .75);
+	        break;	    
+	}
+	
+	$obj[2][] = $trackColor;
+	$obj[2][] = getHSLColor( $dt, "email_address", $maxValues );
+	$obj[2][] = getHSLColor( $dt, "email_domain", $maxValues );	
+	
+	return $obj;
+}, $payload);
+
+
+// $payload = array_slice($payload, 0, 3);
+
+echo json_encode( $payload );
+
+
+function getLetterHSLColor( $letter ){
+	return (ord("z") - ord($letter)) / 25;
+}
+
+function getHSLColor( $dt, $key, $maxValues ){
+        
+    $colorVal = array("h" => 0, "s" => 0, "l" => 0);
+    
+    foreach( $dt[$key] as $index => $value ){
+         
+        $percentMax = $value / $maxValues[$key][$index];
+        $colorKey = "";
+        $hslVal = $percentMax;
+         
+        switch( $index ){
+            case 0:
+                $colorKey = "l";
+                break;
+            case 1: $colorKey = "s";
+            break;
+            case 2: $colorKey = "h";
+            $hslVal = $percentMax * 359;
+            break;
+            default: break;
+        }
+    
+        $colorVal[$colorKey] = $hslVal;
+    }
+    
+    return $colorVal;
+}
---
+++ d3_startup_school/index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="main.css" rel="stylesheet">
+    
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <script src="jquery-1.8.3.min.js"></script>
+    <script src="d3.v3.min.js"></script>
+    <script src="underscore-min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="jquery.color-2.1.1.min.js"></script>
+    
+  </head>
+
+  <body>
+
+    <div class="header">
+        <div class="pull-left">
+            <a href="http://www.setfive.com" target="_blank"><img src="logo_website_no_consulting.png" /></a>
+        </div>
+        <div class="pull-right">
+            <div class="header-menubar">
+                <a href="mailto:contact@setfive.com">contact@setfive.com</a>
+                |
+                <a href="http://www.twitter.com/setfive.com">@setfive</a>
+                |
+                <a href="tel:6178630440">617.863.0440</a>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+
+    <div class="container padded-bottom">
+        <div class="row">
+            <div class="span12 centered">
+                <h2>Welcoming the Startup Institute: Boston class of Spring '13</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="span12">
+                <div class="alert alert-info centered">
+                    We're excited to meet everyone but in the meantime we thought it would be fun to put together a visualization of your class. 
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="span12">
+                <div class="stats-line">
+                    <em class="highlight">Eight</em> members of the class have a first name that starts with "J" and <em class="highlight">two</em> have a first name that starts with "Z". 
+                    <em class="highlight">Two</em> last names that start with "Z" and <em class="highlight">one</em> with "Q". On resumes, @gmail.com appeared <em class="highlight">thirty six</em> times, by far the most popular domain.
+                    <em class="highlight">Thirteen</em> of you are in the dev track, <em class="highlight">seventeen</em> in marketing, <em class="highlight">ten</em> in product, and <em class="highlight">thirteen</em> in sales.                         
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="container padded-bottom">
+        <div class="row">
+            <div class="span12">
+                <div class="page-header">
+                    <h3>Visualizing the class</h3>                   
+                </div>  
+                <p>Each 3x3 grid below represents a member of the class. The top two rows represent the student's first and last names.  
+                In the <strong>first row</strong>, the left column represents the first letter of the first name, 
+                the middle column represents the length of the first name, and the right column is a HSL mapping for a <a href="http://www.php.net/manual/en/function.metaphone.php" target="_blank">metaphone</a> of the first name.
+                The <strong>second row</strong> is identical to the first except mapping the last name.
+                In the <strong>last row</strong>, the first column represents the track the student is in
+                and then the middle left and middle right squares are a HSL representation of the <a href="http://www.php.net/manual/en/function.metaphone.php" target="_blank">metaphone</a> 
+                of the student's email user name and domain name (john.doe, gmail.com).
+                                
+                <div class="pull-left">
+                    <ul class="listless inline">
+                        <li><strong>Filter: </strong></li>
+                        <li><a href="#" class="sales filter-on" data-provide="filter" data-val="sales">Sales</a></li>
+                        <li><a href="#" class="prod filter-on" data-provide="filter" data-val="prod">Product</a></li>
+                        <li><a href="#" class="marketing filter-on" data-provide="filter" data-val="mrkt">Marketing</a></li>
+                        <li><a href="#" class="dev filter-on" data-provide="filter" data-val="dev">Development</a></li>
+                    </ul>                                
+                    <ul class="listless inline">
+                        <li><strong>Sort By:</strong></li>
+                        <li><a class="selected" href="#" data-provide="sort" data-sort="fname_text">First Name</a></li>
+                        <li><a href="#" data-provide="sort" data-sort="lname_text">Last Name</a></li>
+                        <li><a href="#" data-provide="sort" data-sort="track">Track</a></li>
+                    </ul>
+                </div>
+                
+                <div class="pull-right">
+
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+                <div id="svg"></div>
+            </div>
+        </div>
+    </div>
+    
+    <script>
+
+    var svg;
+    var innerSvg;
+    var jqBaseColor = jQuery.Color( "#eee" );
+    
+    var cellSize = 30;
+	var cellBuffer = 32;
+	var cubeSize = (cellBuffer * 1.15) * 3;
+    var cubesPerRow = 8;
+    
+    $(document).ready(function(){
+        
+        getData("fname");
+
+        $("[data-provide='sort']").click(function(){
+            $("[data-provide='sort']").removeClass("selected");
+            $(this).addClass("selected");
+                        
+            getData( $(this).data("sort") );
+            return false;
+        });
+
+        $("[data-provide='filter']").click(function(){
+			if( $(this).hasClass("filter-on") ){
+				$(this).removeClass("filter-on");
+			}else{
+				$(this).addClass("filter-on");
+			}
+
+			getData( $("[data-provide='sort']").filter(".selected").data("sort") );
+        	return false;
+        });
+        
+    });
+
+    function getData(sortBy){
+
+    	$("#svg *").remove();
+    	
+        svg = d3.select("#svg").append("svg")
+		.attr("class", "chart")
+		.attr("width", 930)
+		.attr("height", 900)
+	  .append("g");   
+
+        var filters = [ ];
+        $("[data-provide='filter']").filter(".filter-on").each(function(){
+			filters.push( $(this).data("val") );
+        });
+    	
+        $.getJSON("data.php", {sortBy: sortBy, filters: filters}, function(data){
+
+            /*
+                [
+                     [
+                          {"h":0,"s":0.44311377245509,"l":0.38235294117647},
+                          {"h":170.74390243902,"s":0.65560165975104,"l":0.65271966527197}
+                     ],
+                     [
+                          {"h":168.68674698795,"s":0.94610778443114,"l":0.83529411764706},
+                          {"h":170.74390243902,"s":0.67219917012448,"l":0.61924686192469}
+                     ],
+                     [{"h":0,"s":0.9940119760479,"l":0.84117647058824},{"h":0,"s":0.31535269709544,"l":0.30125523012552}]
+                     
+                ]
+            
+            */
+
+            
+            svg.selectAll('g').data(data).exit().remove();
+            
+            rowSvg = svg.selectAll('g').data(data).enter().append('g').attr("class", "svg-row");
+
+            
+            rowSvg.each(function(el, userIndex){
+                
+				var rowInnerSVG = d3.select(this)
+								    .selectAll("g")
+								      .data(el)
+								      .enter()
+								    .append("g");
+				
+				rowInnerSVG.each(function(ex, step){
+					
+					var rects = d3.select(this)
+								    .selectAll("rect")
+								    .data(ex);
+
+					function setSquareCords( square ){
+						
+					    square.attr('x', function(val, index){
+					            var rowMultiplier = userIndex % cubesPerRow;             
+				                return (cubeSize * rowMultiplier) + (index * cellBuffer);
+				              })
+			                  .attr('y', function(val, index){
+			                    var rowOffset = userIndex >= cubesPerRow ? Math.floor(userIndex / cubesPerRow) * cubeSize : 0;						               
+				                return rowOffset + (step * cellBuffer);
+			                  })
+			                  .attr('width', cellSize)
+			                  .attr('height', cellSize)
+			                  .attr('fill', function(obj, index){
+				                 return jQuery.Color( {hue: obj.h, saturation: obj.s, lightness: obj.l, alpha: 1} ).toHslaString();
+				              })					    
+					}
+
+					rects.transition().duration(750).call(setSquareCords);
+				    rects.enter().append("rect").call(setSquareCords);				    
+				    rects.exit().remove();
+		            
+				});
+								      
+            });
+			
+        });
+
+    }
+    
+    </script>
+    
+   </body>
+</html>
