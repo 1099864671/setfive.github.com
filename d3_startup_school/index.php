3c61ea78841964766586e63aa8d0655e3fac3c36
Ashish Datta <ashish@setfive.com>
2013-03-07 19:58:27 -0500

    added the about the squares block

--- d3_startup_school/index.php
+++ d3_startup_school/index.php
@@ -67,50 +67,96 @@
     </div>
     
     <div class="container padded-bottom">
+    
         <div class="row">
             <div class="span12">
                 <div class="page-header">
                     <h3>Visualizing the class</h3>                   
                 </div>  
+            </div>
+        </div>
+    
+        <div class="row">
+        
+            <div class="span6 inthe-squares centered">
+                <h3>What's in the squares?</h3>
+                <table class="table table-bordered table-striped table-condensed">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="#" title="First Name Letter" data-content="Gradient based on the first letter of first name. (A = 0, Z = 100)" rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                            
+                            <td>
+                                <a href="#" title="First Name Length" data-content="Gradient based on the length of the first name. (A = 0, Z = 100)" rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                            
+                            <td>
+                                <a href="#" title="First Name Metaphone" data-content="A color based on the metaphone of the first name." rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="#" title="Last Name Letter" data-content="Gradient based on the last letter of first name. (A = 0, Z = 100)" rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                            
+                            <td>
+                                <a href="#" title="Last Name Length" data-content="Gradient based on the length of the last name. (A = 0, Z = 100)" rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                            
+                            <td>
+                                <a href="#" title="Last Name Metaphone" data-content="A color based on the metaphone of the last name." rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <a href="#" title="Track" data-content="Starup school track." rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                            
+                            <td>
+                                <a href="#" title="Email Username" data-content="Color based on the email username." rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                            
+                            <td>
+                                <a href="#" title="Email Domain" data-content="Color based on the email domain name." rel="tooltip" class="btn"><i class="icon-question-sign"></i></a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p><strong>Note:</strong> Click the question marks for info!</p>
+            </div>
+        
+            <div class="span6">
                 <p>Each 3x3 grid below represents a member of the class. The top two rows represent the student's first and last names.  
                 In the <strong>first row</strong>, the left column represents the first letter of the first name, 
                 the middle column represents the length of the first name, and the right column is a HSL mapping for a <a href="http://www.php.net/manual/en/function.metaphone.php" target="_blank">metaphone</a> of the first name.
                 The <strong>second row</strong> is identical to the first except mapping the last name.
                 In the <strong>last row</strong>, the first column represents the track the student is in
                 and then the middle left and middle right squares are a HSL representation of the <a href="http://www.php.net/manual/en/function.metaphone.php" target="_blank">metaphone</a> 
-                of the student's email user name and domain name (john.doe, gmail.com).
-                                
-                <div class="pull-left">
-                    <ul class="listless inline">
-                        <li><strong>Filter: </strong></li>
-                        <li><a href="#" class="sales filter-on" data-provide="filter" data-val="sales">Sales</a></li>
-                        <li><a href="#" class="prod filter-on" data-provide="filter" data-val="prod">Product</a></li>
-                        <li><a href="#" class="marketing filter-on" data-provide="filter" data-val="mrkt">Marketing</a></li>
-                        <li><a href="#" class="dev filter-on" data-provide="filter" data-val="dev">Development</a></li>
-                    </ul>                                
-                    <ul class="listless inline">
-                        <li><strong>Sort By:</strong></li>
-                        <li><a class="selected" href="#" data-provide="sort" data-sort="fname_text">First Name</a></li>
-                        <li><a href="#" data-provide="sort" data-sort="lname_text">Last Name</a></li>
-                        <li><a href="#" data-provide="sort" data-sort="track">Track</a></li>
-                    </ul>
-                </div>
-                
-                <div class="pull-right">
-                	<table class="table table-bordered table-striped table-condensed table-key">
-                		<tbody>
-                			<tr><td>A</td><td>M</td><td>Z</td></tr>
-                			<tr>
-                				<td colspan="3">
-                					<div class="letter-gradient">&nbsp;</div>
-                				</td>
-                			</tr>
-                		</tbody>
-                	</table>
-                </div>
-                
+                of the student's email user name and domain name (john.doe, gmail.com).                        
             </div>
         </div>
+        
+        <div class="row">
+            <div class="span12">
+            
+                <ul class="listless inline">
+                    <li><strong>Filter: </strong></li>
+                    <li><a href="#" class="sales filter-on" data-provide="filter" data-val="sales">Sales</a></li>
+                    <li><a href="#" class="prod filter-on" data-provide="filter" data-val="prod">Product</a></li>
+                    <li><a href="#" class="marketing filter-on" data-provide="filter" data-val="mrkt">Marketing</a></li>
+                    <li><a href="#" class="dev filter-on" data-provide="filter" data-val="dev">Development</a></li>
+                </ul>                                
+                <ul class="listless inline">
+                    <li><strong>Sort By:</strong></li>
+                    <li><a class="selected" href="#" data-provide="sort" data-sort="fname_text">First Name</a></li>
+                    <li><a href="#" data-provide="sort" data-sort="lname_text">Last Name</a></li>
+                    <li><a href="#" data-provide="sort" data-sort="track">Track</a></li>
+                </ul> 
+            
+            </div>
+        </div>
+        
     </div>
     
     <div class="container">
@@ -120,6 +166,17 @@
             </div>
         </div>
     </div>
+    
+    <div class="container padded-bottom">
+        <div class="row">
+            <div class="span12">
+    			<div class="alert alert-info centered follow-us-bottom">
+				Dig the visualization? We'd love if you followed us  
+				<iframe scrolling="no" frameborder="0" allowtransparency="true" src="http://platform.twitter.com/widgets/follow_button.1362636220.html#_=1362664812230&amp;id=twitter-widget-0&amp;lang=en&amp;screen_name=setfive&amp;show_count=false&amp;show_screen_name=true&amp;size=m" class="twitter-follow-button twitter-follow-button" style="width: 109px; height: 20px;" title="Twitter Follow Button" data-twttr-rendered="true"></iframe>		 
+				</div>
+			</div>
+		</div>
+	</div>
     
     <script>
 
@@ -132,6 +189,12 @@
     var cubesPerRow = 8;
     
     $(document).ready(function(){
+
+        $(".inthe-squares a").click(function(){
+            return false;
+        });
+        
+        $("[rel='tooltip']").popover();
         
         getData("fname_text");
 
@@ -161,10 +224,10 @@
     	$("#svg *").remove();
     	
         svg = d3.select("#svg").append("svg")
-		.attr("class", "chart")
-		.attr("width", 930)
-		.attr("height", 900)
-	  .append("g");   
+			.attr("class", "chart")
+			.attr("width", 930)
+			.attr("height", 900)
+		  .append("g");   
 
         var filters = [ ];
         $("[data-provide='filter']").filter(".filter-on").each(function(){
@@ -188,12 +251,10 @@
                 ]
             
             */
-
             
             svg.selectAll('g').data(data).exit().remove();
             
             rowSvg = svg.selectAll('g').data(data).enter().append('g').attr("class", "svg-row");
-
             
             rowSvg.each(function(el, userIndex){
                 
